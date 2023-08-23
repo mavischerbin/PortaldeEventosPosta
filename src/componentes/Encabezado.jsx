5bf15684fe62bd222c803ae428559abe4a8165f9
mavischerbin <mavitascherbin12@gmail.com>
2023-08-23 19:23:35 -0300

    actualizar_Enlaces

---
+++ src/componentes/Encabezado.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            {/* <img src={logoPortal} alt="" className='' /> */}
+            <div className='blanco'>srdgsgsdf</div>
+        </div>
+    )
+}
+
+export default Header
